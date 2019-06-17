d13a5dcecf61f032fbcaf595d727e33aebe1f56b
Cody Gillette <cgillette1312@gmail.com>
2019-06-17 09:09:56 -0700

    fun games

--- HashMap_drills.js
+++ HashMap_drills.js
@@ -28,3 +28,26 @@ function main(){
 // capacity 24, once capacty hit its initial cap of 8 it tripled its size. 
 main();
 
+
+// 2 this function is taking in key words and seting there value to a
+// diffenrent number that is being stored in the hash map;
+
+
+const WhatDoesThisDo = function () {
+  let str1 = 'Hello World.';
+  let str2 = 'Hello World.';
+  let map1 = new HashMap();
+  map1.set(str1, 10);
+  map1.set(str2, 20);
+  let map2 = new HashMap();
+  let str3 = str1;
+  let str4 = str2;
+  map2.set(str3, 20);
+  map2.set(str4, 10);
+
+  console.log(map1.get(str1));
+  console.log(map2.get(str3));
+};
+
+// console.log(WhatDoesThisDo());
+
